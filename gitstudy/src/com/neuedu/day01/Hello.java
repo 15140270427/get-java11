de3cdb7585685178b77a0909e0d6fbd1654355e0
15140270427 <834213439@qq.com>
2020-08-19 16:33:18 +0800

    156

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -4,15 +4,13 @@ public class Hello {
 
        public static void main(String[] args) {
 		System.out.println("hellogril");
-
 	    System.out.println("hellogril1");
 	    System.out.println("hello3");
-
 	       System.out.println("hellogril1");
 	       System.out.println("----------");
 	       System.out.println("hello1");
 	       System.out.println("hello2");
-
+	       System.out.println("hello4");
 	}
 	
 	
